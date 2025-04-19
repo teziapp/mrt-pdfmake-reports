8bca4f636c8febdd0092a58a55646a80de56fc95
smarty <mohit.patel1966@gmail.com>
2025-04-19 23:11:22 +0530

    app tsx removed

--- test-lib/src/main.tsx
+++ test-lib/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { SmartTableDemo } from './SmartTableDemo';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SmartTableDemo />
   </React.StrictMode>
 ); 
