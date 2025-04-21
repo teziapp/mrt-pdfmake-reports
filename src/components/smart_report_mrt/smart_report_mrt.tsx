ff5bafa27da97870988955d1af234729e14a00a9
abbasmithaiwala <mithaiwalaabbas1@gmail.com>
2025-04-21 21:03:57 +0530

    Fixed dev dep issues

--- src/components/smart_report_mrt/smart_report_mrt.tsx
+++ src/components/smart_report_mrt/smart_report_mrt.tsx
@@ -1,30 +1,24 @@
+import TableViewIcon from '@mui/icons-material/TableView';
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import { Button } from "@mui/material";
 import { MaterialReactTable, MaterialReactTableProps, MRT_RowData } from "material-react-table";
 import { useState } from "react";
 import "./smart_report_mrt.css";
-import { ToolbarComponentProps, defaultToolbarComponents } from "./toolbar-components";
-
+import { handleDownloadPdf } from "./handleDownloadPdf";
 /*
   TODO: (Responsive Card View)
   - Add a field `showInCardView` to the column definition. Default to false.
   - Show columns that have `showInCardView` set to true in card view.
   - When user toggles visibility of a column in card view, update the `showInCardView` field.
 */
-
-type SmartReportMRTProps<T extends MRT_RowData> = MaterialReactTableProps<T> & {
-  toolbarComponents?: Array<React.ComponentType<ToolbarComponentProps>>;
-};
-
-export const SmartReportMRT = <T extends MRT_RowData>({ 
-  toolbarComponents = defaultToolbarComponents,
-  ...props
-}: SmartReportMRTProps<T>) => {
+export const SmartReportMRT = <T extends MRT_RowData>(props: MaterialReactTableProps<T>) => {
   // State to control the view mode override. Default to table view on desktop & card view on mobile.
   const [forceTableView, setForceTableView] = useState(window.innerWidth >= 500);
-
   return (
     <MaterialReactTable
       // Add props to the main table element (<table>)
-      muiTableProps={{
+      muiTableProps = {{
         // Conditionally add the 'force-table-view' class
         className: `responsive-card-table ${forceTableView ? 'force-table-view' : ''}`,
         sx: {
@@ -33,20 +27,26 @@ export const SmartReportMRT = <T extends MRT_RowData>({
       }}
       // Add props to the table body cells (<td>)
       // @ts-expect-error FIXME: Redeclare / Redfine / Extend `TableCellProps` to add `data-label` prop.
-      muiTableBodyCellProps={({ cell }) => ({
+      muiTableBodyCellProps = {({ cell }) => ({
         'data-label': cell.column.columnDef.header,
       })}
-      renderTopToolbar={() => (
+      renderTopToolbar = {() => (
         <div className="top-toolbar">
-          {toolbarComponents.map((Component, index) => (
-            <Component
-              key={index}
-              forceTableView={forceTableView}
-              setForceTableView={setForceTableView}
-            />
-          ))}
+          <Button 
+            onClick={() => setForceTableView(!forceTableView)}
+            startIcon={forceTableView ? <ViewModuleIcon /> : <TableViewIcon />}
+          >
+            {forceTableView ? 'Switch to Card View' : null}
+          </Button>
+          <Button 
+            onClick={handleDownloadPdf}
+            startIcon={<PictureAsPdfIcon />}
+          >
+            Download PDF
+          </Button>
         </div>
       )}
+      
       {...props}
     />
   );
