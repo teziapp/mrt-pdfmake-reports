778553033445428efbabf945fd55329b661c355e
abbasmithaiwala <mithaiwalaabbas1@gmail.com>
2025-04-22 14:56:13 +0530

    Added proper passing of headers

--- src/pdf/components/headerImageSection.ts
+++ src/pdf/components/headerImageSection.ts
@@ -1,16 +1,22 @@
 import { ContentImage } from 'pdfmake/interfaces';
 import { checkImageValidGetDef } from '../utils/fetchValidImageURL';
 
+interface ImageHeaders {
+  [key: string]: string;
+}
+
 export const headerImageSection = (
   inputImageDef: ContentImage,
   imageUrl: string,
-  customStyle?: ContentImage
+  customStyle?: ContentImage,
+  headers?: ImageHeaders
 ) => {
   return checkImageValidGetDef(
     {
       ...inputImageDef,
       style: customStyle
     }, 
-    imageUrl
+    imageUrl,
+    headers
   );
 }
