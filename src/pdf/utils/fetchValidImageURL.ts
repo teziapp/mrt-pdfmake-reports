9c015ba4839c65daf0bb735f0a38fe4b0490710b
abbasmithaiwala <mithaiwalaabbas1@gmail.com>
2025-04-22 13:46:14 +0530

    Added image headers functionality

--- src/pdf/utils/fetchValidImageURL.ts
+++ src/pdf/utils/fetchValidImageURL.ts
@@ -4,19 +4,29 @@ type CustomContentImage =
 | (Omit<ContentImage, 'image' | 'text'> & { text: string; image?: never })
 | (Omit<ContentImage, 'image' | 'text'> & { text?: never; image: string });
 
+interface ImageHeaders {
+	[key: string]: string;
+}
 
 export const checkImageValidGetDef = async (
 	inputImageDef: ContentImage,
-	imageUrl: string
+	imageUrl: string,
+	headers?: ImageHeaders
 ) => {
-	const validUrl = await fetch(imageUrl)
+	const validUrl = await fetch(imageUrl, {
+		headers: headers || {}
+	})
 	.then(() => imageUrl)
 	.catch(() => '');
 
 	if (validUrl) {
+		const imageValue = headers 
+			? { url: validUrl, headers } 
+			: validUrl;
+
 		return {
 			imageDef: inputImageDef as CustomContentImage,
-			image: { [inputImageDef.image]: validUrl },
+			image: { [inputImageDef.image]: imageValue },
 		}
 	} else {
 		const { image, ...imageDef } = inputImageDef
