c2da11ee7d2ce27bd1ae2a458adee1717d7033ab
smarty <mohit.patel1966@gmail.com>
2025-04-27 14:04:33 +0530

    smart table settings added

--- src/components/smart_report/smart_report.tsx
+++ src/components/smart_report/smart_report.tsx
@@ -1,6 +1,7 @@
 import { MaterialReactTableProps, MRT_RowData, MRT_ColumnDef, MRT_TableInstance } from "material-react-table";
 import { TableContextProvider } from "../../TableContextProvider";
 import { SmartReportMRT } from "../smart_report_mrt/smart_report_mrt";
+import { SmartTableSettings } from "../smart_table_settings/smart_table_settings";
 type SmartReportProps<T extends MRT_RowData> = {
   tableProps: Omit<MaterialReactTableProps<T>, 'columns' | 'data'> & {
     columns: MRT_ColumnDef<T>[];
@@ -16,6 +17,7 @@ export const SmartReport = <T extends MRT_RowData>({
   
   return (
     <TableContextProvider tableProps={tableProps} table={table}>
+        <SmartTableSettings />
         <SmartReportMRT />
     </TableContextProvider>
   );
